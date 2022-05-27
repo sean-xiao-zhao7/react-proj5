6deeb6ed671a1b95928764cf7a1fb1ab6c818b64
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-05-27 17:19:14 -0400

    Fetch available meals from API

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,52 +1,73 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errors, setErrors] = useState();
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+    useEffect(() => {
+        const getMeals = async () => {
+            try {
+                const res = await fetch(process.env.REACT_APP_FB);
+                if (!res) {
+                    console.log(res);
+                    throw new Error("Server error.");
+                }
+                const resData = await res.json();
+                const resDataMeals = [];
+                for (const key in resData) {
+                    resDataMeals.push({
+                        id: key,
+                        name: resData[key]["name"],
+                        description: resData[key]["description"],
+                        price: resData[key]["price"],
+                    });
+                }
+                setMeals(resDataMeals);
+            } catch (err) {
+                setErrors("Server error.");
+            }
+        };
+        getMeals();
+        setIsLoading(false);
+    }, [setMeals]);
+
+    if (isLoading) {
+        return (
+            <section className={classes.meals}>
+                <Card>Loading.</Card>
+            </section>
+        );
+    }
+
+    if (errors) {
+        return (
+            <section className={classes.meals}>
+                <Card>{errors}</Card>
+            </section>
+        );
+    }
+
+    const mealsList = meals.map((meal) => (
+        <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
+
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
+        </section>
+    );
 };
 
 export default AvailableMeals;
